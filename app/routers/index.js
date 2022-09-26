4a42d5fd52fd94dc54fcf52c00da2a51f4f48e04
Jean-Leherle <j.leherle@gmail.com>
2022-09-26 11:09:09 +0200

    :gear: config: passage de res.send en res.json

--- app/routers/index.js
+++ app/routers/index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', (req, res) => {
-  res.send('hello world');
+  res.json('hello world');
 });
 module.exports = router;
